ac9249ce1ce2c6ec82ba4ddd2de195693754519e
anri varshanidze <anrivarshanidze2407@gmail.com>
2024-07-11 00:52:27 +0400

    change board

--- src/users/views/Dashboard.jsx
+++ src/users/views/Dashboard.jsx
@@ -8,7 +8,10 @@ function Dashboard() {
         <div className="w-[80vw] m-auto flex">
             <CategoryList />
             <div className="p-10 w-full border-l-2 border-stone-200">
-                board
+                <div className="bg-black w-full h-96"></div>
+                <div className="bg-green-500 w-full h-96"></div>
+                <div className="bg-blue-500 w-full h-96"></div>
+                <div className="bg-yellow-500 w-full h-96"></div>
             </div>
         </div>
     )
