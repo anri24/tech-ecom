110f6634bfbeb47864a42b060869191ca7f1f813
anri varshanidze <anrivarshanidze2407@gmail.com>
2024-07-14 20:54:48 +0400

    add register page style

--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -1,7 +1,38 @@
+import { Link } from "react-router-dom"
+import Button from "../users/components/Button"
+
 function Signup() {
     return (
-        <div>
-            sign up
+        <div className="w-[90vw] m-auto">
+            <div className="my-10 flex gap-60 ">
+                
+                <div className="bg-blue-400 w-[50%] h-[70vh] "></div>
+                <div className="flex flex-col gap-5 justify-center">
+                    <h2 className="text-3xl font-bold">Create an account</h2>
+                    <p className="text-sm">Enter your details below</p>
+                    <form className="flex flex-col gap-10">
+                    <div className="flex flex-col">
+                            <label>Name</label>
+                            <input type="text" className="border-b-2 border-stone-300 p-2 bg-stone-200 rounded" />
+                        </div>
+                        <div className="flex flex-col">
+                            <label>Email or Phone Number</label>
+                            <input type="text" className="border-b-2 border-stone-300 p-2 bg-stone-200 rounded" />
+                        </div>
+                        <div className="flex flex-col">
+                            <label>Password</label>
+                            <input type="password"  className="border-b-2 border-stone-300 p-2 bg-stone-200 rounded" />
+                        </div>
+                        <div className="flex flex-col gap-10 text-center">
+                            <Button>Create account</Button>
+                            <div className="flex justify-around">
+                                <span>Already have account?</span>
+                                <Link className="border-b-2 border-stone-400">Log in</Link>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
