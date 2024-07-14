95d570d7b4976440e54f89bbc19eb4ed8d64a0f7
anri varshanidze <anrivarshanidze2407@gmail.com>
2024-07-14 20:49:31 +0400

    create login pages style

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,7 +1,31 @@
+import { Link } from "react-router-dom"
+import Button from "../users/components/Button"
+
 function Login() {
     return (
-        <div>
-            login
+        <div className="w-[90vw] m-auto">
+            <div className="my-10 flex gap-60 ">
+                
+                <div className="bg-blue-400 w-[50%] h-[70vh] "></div>
+                <div className="flex flex-col gap-5 justify-center">
+                    <h2 className="text-3xl font-bold">Log in to Exclusive</h2>
+                    <p className="text-sm">Enter your details below</p>
+                    <form className="flex flex-col gap-10">
+                        <div className="flex flex-col">
+                            <label>Email or Phone Number</label>
+                            <input type="text" className="border-b-2 border-stone-300 p-2 bg-stone-200 rounded" />
+                        </div>
+                        <div className="flex flex-col">
+                            <label>Password</label>
+                            <input type="password"  className="border-b-2 border-stone-300 p-2 bg-stone-200 rounded" />
+                        </div>
+                        <div className="flex gap-40">
+                            <Button>Login</Button>
+                            <Link className="flex flex-col justify-center text-red-500">Forget Password?</Link>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/users/components/Button.jsx
@@ -0,0 +1,7 @@
+function Button({children}) {
+    return (
+        <button type="submit" className=" bg-red-500 px-10 py-3 text-white rounded capitalize">{children}</button>
+    )
+}
+
+export default Button
