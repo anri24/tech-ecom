ff372f0e118a2f1ad34daf2591097056f884d925
anri varshanidze <anrivarshanidze2407@gmail.com>
2024-07-14 22:00:08 +0400

    add about page styles

--- src/users/components/Guaranties.jsx
+++ src/users/components/Guaranties.jsx
@@ -10,21 +10,21 @@ function Guaranties() {
                 <div className="text-3xl p-2 bg-black rounded-full w-12 h-12 text-white flex justify-center"><FaShippingFast /></div>
             </div>
             <div>FREE AND FAST DELIVERY</div>
-            <div>Free delivery for all orders over $140</div>
+            <div>Free delivery for all orders</div>
         </div>
         <div className="flex flex-col gap-2">
             <div className="bg-stone-400 p-2 rounded-full w-16 self-center">
                 <div className="text-3xl p-2 bg-black rounded-full w-12 h-12 text-white flex justify-center"><FaHeadset /></div>
             </div>
-            <div>FREE AND FAST DELIVERY</div>
-            <div>Free delivery for all orders over $140</div>
+            <div>24/7 CUSTOMER SERVICE</div>
+            <div>Friendly 24/7 customer support</div>
         </div>
         <div className="flex flex-col justify-center gap-2">
             <div className="bg-stone-400 p-2 rounded-full w-16 self-center">
                 <div className="text-3xl p-2 bg-black rounded-full w-12 h-12 text-white flex justify-center"><IoShieldCheckmarkOutline /></div>
             </div>
-            <div>FREE AND FAST DELIVERY</div>
-            <div>Free delivery for all orders over $140</div>
+            <div>MONEY BACK GUARANTEE</div>
+            <div>We reurn money within 30 days</div>
         </div>
     </div>
     )
--- src/users/views/About.jsx
+++ src/users/views/About.jsx
@@ -1,7 +1,13 @@
+
 function About() {
     return (
-        <div>
-            about
+        <div className="w-[80vw] mx-auto">
+            <div>
+                <div className="w-[50vw] mx-auto py-20 flex flex-col gap-10">
+                    <h2 className="text-5xl flex justify-center">My Story</h2>
+                    <p className="text-wrap text-2xl">Highly skilled Full-stack Developer with over 2 years of experience in developing and managing web applications using PHP, Laravel, MySQL, and other modern web technologies. Proficient in both client-side and server-side development with a strong focus on delivering high-quality code. </p>
+                </div>
+            </div>
         </div>
     )
 }
