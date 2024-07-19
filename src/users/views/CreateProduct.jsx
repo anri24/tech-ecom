56ef90cc82d589d1d7eabf916753f0999ed8339d
anri varshanidze <anrivarshanidze2407@gmail.com>
2024-07-19 07:23:51 +0400

    added getting categories

--- src/users/views/CreateProduct.jsx
+++ src/users/views/CreateProduct.jsx
@@ -1,19 +1,50 @@
+import { useState } from "react"
 import Button from "../components/Button"
+import { useEffect } from "react";
+import axiosClient from "../../axios-client";
 
 function CreateProduct() {
+    const [categories, setCategories] = useState([]);
+    const [subCategories, setSubCategories] = useState([]);
+    const [selectedCategories, setSelectedCategories] = useState(null);
+
+    useEffect(() => {
+        axiosClient.get('/category/all')
+        .then(({data}) => {
+            setCategories(data.data)
+        })
+    }, []);
+
+    useEffect(() => {
+        if(selectedCategories){
+            axiosClient.get(`/category/${selectedCategories}`)
+            .then(({data}) => {
+                setSubCategories(data.data.children);
+            })
+        }
+    }, [selectedCategories])
+
+    console.log(subCategories)
+
     return (
         <div>
             <form className="w-60 mx-auto flex flex-col gap-5 my-10"> 
                 <div className="flex flex-col">
                     <label>Category</label>
-                    <select className="bg-stone-200 p-2 rounded w-full">
+                    <select className="bg-stone-200 p-2 rounded w-full" value={selectedCategories ? selectedCategories : ''} onChange={(e) => setSelectedCategories(e.target.value)}>
                         <option>Select Category</option>
+                        {categories.map(cat => (
+                            <option key={cat.id} value={cat.id}>{cat.name}</option>
+                        ))}
                     </select>
                 </div>
                 <div className="flex flex-col">
                     <label>SubCategory</label>
-                    <select className="bg-stone-200 p-2 rounded w-full">
+                    <select disabled={!selectedCategories} className="bg-stone-200 p-2 rounded w-full">
                         <option>Select SubCategory</option>
+                        {subCategories.map(subcat => (
+                            <option key={subcat.id} value={subcat.id}>{subcat.name}</option>
+                        ))}
                     </select>
                 </div>
                 <div className="flex flex-col">
